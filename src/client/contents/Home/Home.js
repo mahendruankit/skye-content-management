60b596bdacbcbbfb7fc1018ca18ad997c01a7b5a
Ankit Mahendru <ankit.mahendru@gmail.com>
2021-12-25 22:45:56 +0000

    add homepage text

--- src/client/contents/Home/Home.js
+++ src/client/contents/Home/Home.js
@@ -4,7 +4,12 @@ import './Home.scss';
 export default function Home() {
   return (
     <div className='content home'>
-      <h1 className='home__title'>Home Page</h1>
+      <h1 className='home__title'>Skye Content Management</h1>
+      <p>
+        We deals with instructional design and content management for
+        educational purposes. It ranges from schools, universities, training
+        educators, life skill development for professionals.
+      </p>
     </div>
   );
 }
